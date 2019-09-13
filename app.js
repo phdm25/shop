43f8afa6ab35149a04d766398294fb08365187e4
Dmitrii Filippov <d.filippov@polymatica.com>
2019-09-13 13:43:38 +0300

    user route

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
 
@@ -23,7 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
-app.use('/users', usersRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
 
 // catch 404 and forward to error handler
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf shop' });
+});
+
+/* GET register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register')
+});
+
+/* GET register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register')
+});
+
+/* GET login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login')
+});
+
+/* POST login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login')
+});
+
+/* GET profile  */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile')
+});
+
+/* PUT profile */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id')
+});
+
+/* GET /forgot-pwd  */
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot-password')
+});
+
+/* PUT /forgot  */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot-password')
+});
+
+/* GET /reset  */
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset-password')
+});
+
+/* PUT /reset  */
+router.put('/reset/:token', (req, res, next) => {
+  res.send('PUT /reset-password')
 });
 
 module.exports = router;
